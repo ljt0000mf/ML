a1834152538986838f1285c2d1bba9f628c7715a
ljt0000mf <ljt0000mf@163.com>
2020-06-14 23:24:06 +0800

    add utf-8

--- pytorch/crawler.py
+++ pytorch/crawler.py
@@ -1,6 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+params = {
+        'enc': 'ISO-8859-1'
+    }
+
+base_url = 'http://www.310win.com/'
+
+"""
 url = 'http://www.310win.com/buy/jingcai.aspx?typeID=105&oddstype=2&date=2020-6-4'
 strhtml = requests.get(url)        #Get方式获取网页数据
 #print(strhtml.text)
@@ -8,3 +15,26 @@ soup = BeautifulSoup(strhtml.text, 'lxml')
 #data = soup.select('#main>div>div.mtop.firstMod.clearfix>div.centerBox>ul.newsList>li>a')
 data = soup.select('#MatchTable')
 print(data)
+# print(soup)
+"""
+#url2 = 'http://www.310win.com/analysis/1745162.htm'
+#strhtml2 = requests.get(url2)
+#strhtml2.encoding='utf-8'
+#print(strhtml2.encoding)
+
+#soup2 = BeautifulSoup(strhtml2.text, 'lxml')
+#soup2 = BeautifulSoup(strhtml2.text, 'lxml')
+#print(soup2)
+
+def main():
+    url = base_url+'buy/jingcai.aspx?typeID=105&oddstype=2&date=2020-6-4'
+    strhtml = requests.get(url)
+    strhtml.encoding = 'utf-8'
+    soup = BeautifulSoup(strhtml.text, 'lxml')
+    data = soup.select('#MatchTable')
+    data.select('')
+    print(data)
+
+if __name__ == '__main__':
+    main()
+
