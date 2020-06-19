44743384a1d30e962ca8801a01bfc416a7dc1be8
Jiating <ljt0000mf@163.com>
2020-06-19 16:19:04 +0800

    update

--- pytorch/ball.py
+++ pytorch/ball.py
@@ -1,56 +1,101 @@
 import numpy as np
 import lightgbm as lgb
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 ## load data
 root = 'D:\\AI\\ball\\'
-train_data = pd.read_table(root+'train.txt')
-val_data = pd.read_table(root+'val.txt')
+# train_data = pd.read_table(root+'train3.txt') 2 value
+train_data = pd.read_table(root+'3train.txt')  # 3 value
 #val_data = pd.read_csv(root+'val.csv')
-num_round = 10
+predict_data = pd.read_table(root+'predict.txt')
 
-# traindata = train_data[['s','p','f']]
-# label = train_data[['label']]
 
-# valdata = val_data[['s','p','f']]
-# vallabel = val_data[['label']]
 
 trainlabel = np.array(train_data.label)
 traindata = np.array(train_data.drop("label", axis=1))
 
-vallabel = np.array(val_data.label)
-valdata = np.array(val_data.drop("label", axis=1))
+predictdata = np.array(predict_data)
 
-print(traindata)
-print(trainlabel)
+# vallabel = np.array(val_data.label)
+# valdata = np.array(val_data.drop("label", axis=1))
+
+# print(traindata.shape)
+# print(trainlabel.shape)
+
+x_train,x_val,y_train,y_val = train_test_split(traindata,trainlabel, test_size=0.2,shuffle=True)
+
+lgb_train = lgb.Dataset(x_train, y_train)
+lgb_val = lgb.Dataset(x_val, y_val, reference=lgb_train)
 
 
-lgb_train = lgb.Dataset(traindata, trainlabel, free_raw_data=False)
-lgb_val = lgb.Dataset(valdata, vallabel, free_raw_data=False)
 
+"""
 params = {
     'boosting_type': 'gbdt',
     'objective': 'binary',
-    'metric': 'binary_logloss',
+    'metric': {'l2'},
+    # metric': {'l2', 'l1'},
+    'num_leaves': 60,
+    # 'metric': 'binary_logloss',
     'boost_from_average': 'true',
     'learning_rate': 0.01,
-    'num_class': 1
+    # 'early_stopping_rounds': 500
+    'feature_fraction': 0.9,
+    'bagging_fraction': 0.8,
+    'bagging_freq': 5,
+}
+"""
+params = {
+    'boosting_type': 'dart',
+    'objective': 'multiclass',
+    'num_leaves': 60,
+    'metric': 'multi_logloss',
+    'boost_from_average': 'true',
+    'learning_rate': 0.01,
+    'num_class': 3,
+    'feature_fraction': 0.9,
+    'bagging_fraction': 0.8,
+    'bagging_freq': 5,
 }
 
-print("开始训练")
+def train():
+    print("开始训练")
+    """
+    gbm = lgb.train(params,  # 参数字典
+                    lgb_train,  # 训练集
+                    num_boost_round=1000,  # 迭代次数
+                    valid_sets=lgb_val,  # 验证集
+                    early_stopping_rounds=50  # 早停系数
+                    )
+    """
+    lgb.cv(params,  # 参数字典
+            lgb_train,  # 训练集
+            num_boost_round=1000,  # 迭代次数
+            nfold=5
+            )
+    # gbm.save_model(root+'model.txt')
 
-gbm = lgb.train(params,                     # 参数字典
-                lgb_train,                  # 训练集
-                num_boost_round=20,       # 迭代次数
-                valid_sets=lgb_val,        # 验证集
-                #early_stopping_rounds=30    # 早停系数
-                )
 
-gbm.save_model('model.txt')
+def predict():
+    model_file = root + 'model.txt'
+    gbm = lgb.Booster(model_file=model_file)
+
+    print('开始预测...')
+    y_predt = gbm.predict(predictdata, num_iteration=gbm.best_iteration)
+    # print(y_predt)
+    # y_preds = [ 1 if i >=0.5 else 0 for i in y_predt]  # 2 value
+    y_preds = [list(x).index(max(x)) for x in y_predt]  # 3 value
+
+    print(y_preds)
+
+    dt = pd.DataFrame(y_preds)
+    dt.to_csv(root + "0619_2result.csv", encoding='utf_8_sig')
+
+def main():
+    train()
+    #predict()
 
 
-ypred = gbm.predict(valdata)
-
-print(ypred)
-""""""
-
+if __name__ == '__main__':
+    main()
--- pytorch/crawler.py
+++ pytorch/crawler.py
@@ -1,39 +1,288 @@
 import requests
 from bs4 import BeautifulSoup
-
-params = {
-        'enc': 'ISO-8859-1'
-    }
+import re
+import pymssql
+import datetime
+import time
+import pandas as pd
 
 base_url = 'http://www.310win.com/'
+Headers = {'User-Agent': "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)"}
+CUP = '杯'
+JB = '锦标'
+END = '完场'
 
-"""
-url = 'http://www.310win.com/buy/jingcai.aspx?typeID=105&oddstype=2&date=2020-6-4'
-strhtml = requests.get(url)        #Get方式获取网页数据
-#print(strhtml.text)
-soup = BeautifulSoup(strhtml.text, 'lxml')
-#data = soup.select('#main>div>div.mtop.firstMod.clearfix>div.centerBox>ul.newsList>li>a')
-data = soup.select('#MatchTable')
-print(data)
-# print(soup)
-"""
-#url2 = 'http://www.310win.com/analysis/1745162.htm'
-#strhtml2 = requests.get(url2)
-#strhtml2.encoding='utf-8'
-#print(strhtml2.encoding)
+class MatchResult:
+    home_all_total_match = 0
+    home_all_total_get_goal = 0
+    home_all_total_loss_goal = 0
+    home_all_total_score = 0
+    home_all_total_get_goal_rate = 0.0
+    home_all_total_loss_goal_rate = 0.0
 
-#soup2 = BeautifulSoup(strhtml2.text, 'lxml')
-#soup2 = BeautifulSoup(strhtml2.text, 'lxml')
-#print(soup2)
+    home_total_match = 0
+    home_total_get_goal = 0
+    home_total_loss_goal = 0
+    home_total_score = 0
+    home_total_get_goal_rate = 0.0
+    home_total_loss_goal_rate = 0.0
 
-def main():
-    url = base_url+'buy/jingcai.aspx?typeID=105&oddstype=2&date=2020-6-4'
-    strhtml = requests.get(url)
+    guest_all_total_match = 0
+    guest_all_total_get_goal = 0
+    guest_all_total_loss_goal = 0
+    guest_all_total_score = 0
+    guest_all_total_get_goal_rate = 0.0
+    guest_all_total_loss_goal_rate = 0.0
+
+    guest_total_match = ''
+    guest_total_get_goal = ''
+    guest_total_loss_goal = ''
+    guest_total_score = ''
+    guest_total_get_goal_rate = 0.0
+    guest_total_loss_goal_rate = 0.0
+
+    seq = ''
+    league_title = ''
+    home_team = ''
+    guest_team = ''
+    score = ''
+    result = 0
+    match_day = ''
+
+
+
+def has_attr_cansale(tag):
+    return tag.has_attr('cansale')
+
+
+def has_attr_detail(bgcolor):
+    return bgcolor and re.compile("#FFECEC").search(bgcolor)
+
+
+def getresult(score):
+    tmpscore = score.split('-')
+    tmp = int(tmpscore[0]) - int(tmpscore[1])
+    if tmp == 0:
+        return 1
+    elif tmp > 0:
+        return 2
+
+    return 0
+
+
+def conn():
+    connect = pymssql.connect(server='192.168.90.76', user='tdms_user', password='tdms_user@123!', database='TDMS_SY') #服务器名,账户,密码,数据库名
+    if connect:
+        print("连接成功!")
+    return connect
+
+
+def getdetail(url):
+    strhtml = requests.get(url, headers=Headers)
     strhtml.encoding = 'utf-8'
     soup = BeautifulSoup(strhtml.text, 'lxml')
-    data = soup.select('#MatchTable')
-    data.select('')
-    print(data)
+    home_trs = soup.find_all("tr", align='middle', bgcolor='#FFECEC')
+    if len(home_trs) == 0:
+        return None
+
+    matchResult = MatchResult()
+    # if home_trs[0].contents[3].text == '' or int(home_trs[0].contents[3].text) < 6:
+    if home_trs[0].contents[3].text == '\xa0' or int(home_trs[0].contents[3].text) < 6:
+        # print('home_trs', 'yes')
+        return None  # home less six match will be ignore or no data
+
+    matchResult.home_all_total_match = home_trs[0].contents[3].text
+    matchResult.home_all_total_get_goal = home_trs[0].contents[11].text
+    matchResult.home_all_total_loss_goal = home_trs[0].contents[13].text
+    matchResult.home_all_total_score = home_trs[0].contents[15].text
+    matchResult.home_all_total_get_goal_rate = round(float(matchResult.home_all_total_get_goal)/float(matchResult.home_all_total_match), 2)
+    matchResult.home_all_total_loss_goal_rate = round(float(matchResult.home_all_total_loss_goal)/float(matchResult.home_all_total_match), 2)
+
+    if home_trs[1].contents[3].text == '0':
+        return None  # data error
+    matchResult.home_total_match = home_trs[1].contents[3].text
+    matchResult.home_total_get_goal = home_trs[1].contents[11].text
+    matchResult.home_total_loss_goal = home_trs[1].contents[13].text
+    matchResult.home_total_score = home_trs[1].contents[15].text
+    matchResult.home_total_get_goal_rate = round(float(matchResult.home_total_get_goal)/float(matchResult.home_total_match), 2)
+    matchResult.home_total_loss_goal_rate = round(float(matchResult.home_total_loss_goal)/float(matchResult.home_total_match), 2)
+
+    guest_trs = soup.find_all("tr", align='middle', bgcolor='#CCCCFF')
+    if guest_trs[0].contents[3].text == '\xa0' or int(guest_trs[0].contents[3].text) < 6:
+        # print('home_trs', 'yes')
+        return None  # guest less six match will be ignore or no data
+    matchResult.guest_all_total_match = guest_trs[0].contents[3].text
+    matchResult.guest_all_total_get_goal = guest_trs[0].contents[11].text
+    matchResult.guest_all_total_loss_goal = guest_trs[0].contents[13].text
+    matchResult.guest_all_total_score = guest_trs[0].contents[15].text
+
+    matchResult.guest_all_total_get_goal_rate = round(
+        float(matchResult.guest_all_total_get_goal) / float(matchResult.guest_all_total_match), 2)
+    matchResult.guest_all_total_loss_goal_rate = round(
+        float(matchResult.guest_all_total_loss_goal) / float(matchResult.guest_all_total_match), 2)
+
+    matchResult.guest_total_match = guest_trs[2].contents[3].text
+    matchResult.guest_total_get_goal = guest_trs[2].contents[11].text
+    matchResult.guest_total_loss_goal = guest_trs[2].contents[13].text
+    matchResult.guest_total_score = guest_trs[2].contents[15].text
+    matchResult.guest_total_get_goal_rate = round(
+        float(matchResult.guest_total_get_goal) / float(matchResult.guest_total_match), 2)
+    matchResult.guest_total_loss_goal_rate = round(
+        float(matchResult.guest_total_loss_goal) / float(matchResult.guest_total_match), 2)
+
+    return matchResult
+
+
+def savetoDB(matchResultList):
+    sql = 'insert into ball(match_day,seq,league_title,home_team,guest_team,score,result,home_all_total_match,home_all_total_get_goal,\
+    home_all_total_loss_goal,home_all_total_score,home_all_total_get_goal_rate,home_all_total_loss_goal_rate,\
+    home_total_match,home_total_get_goal,home_total_loss_goal,home_total_score,home_total_get_goal_rate,\
+    home_total_loss_goal_rate,guest_all_total_match,guest_all_total_get_goal,guest_all_total_loss_goal,\
+    guest_all_total_score,guest_all_total_get_goal_rate,guest_all_total_loss_goal_rate,guest_total_match,\
+    guest_total_get_goal,guest_total_loss_goal,guest_total_score,guest_total_get_goal_rate,guest_total_loss_goal_rate) \
+    values(%s, %s, %s, %s, %s, %s,%d, %d, %d, %d, %d, %s, %s, %d, %d, %d, %d, %s, %s, %d, %d, %d, %d, %s, %s, %d, %d, %d, %d, %s, %s)'
+
+    data = []
+    for matchResult in matchResultList:
+        data .append((matchResult.match_day, matchResult.seq, matchResult.league_title
+                      , matchResult.home_team, matchResult.guest_team, matchResult.score
+                      , matchResult.result, matchResult.home_all_total_match, matchResult.home_all_total_get_goal
+                      , matchResult.home_all_total_loss_goal, matchResult.home_all_total_score, matchResult.home_all_total_get_goal_rate
+                      , matchResult.home_all_total_loss_goal_rate, matchResult.home_total_match
+                      , matchResult.home_total_get_goal, matchResult.home_total_loss_goal, matchResult.home_total_score
+                      , matchResult.home_total_get_goal_rate, matchResult.home_total_loss_goal_rate, matchResult.guest_all_total_match
+                      , matchResult.guest_all_total_get_goal, matchResult.guest_all_total_loss_goal, matchResult.guest_all_total_score
+                      , matchResult.guest_all_total_get_goal_rate, matchResult.guest_all_total_loss_goal_rate, matchResult.guest_total_match
+                      , matchResult.guest_total_get_goal, matchResult.guest_total_loss_goal, matchResult.guest_total_score
+                      , matchResult.guest_total_get_goal_rate, matchResult.guest_total_loss_goal_rate))
+    #print(data)
+    con = conn()
+    cursor = con.cursor()
+    cursor.executemany(sql, data)
+    con.commit()
+    con.close()
+
+
+def get_result(fyear, fmonth, fday, tyear, tmonth, tday):
+    begin = datetime.date(fyear, fmonth, fday)
+    end = datetime.date(tyear, tmonth, tday)
+    for i in range((end - begin).days + 1):
+        day = begin + datetime.timedelta(days=i)
+        day = str(day)
+        url = base_url + 'buy/jingcai.aspx?typeID=105&oddstype=2&date=' + day
+        strhtml = requests.get(url, headers=Headers)
+        strhtml.encoding = 'utf-8'
+        soup = BeautifulSoup(strhtml.text, 'lxml')
+        matchResultList = []
+        for tr in soup.find_all(has_attr_cansale):
+            seq = tr.contents[1].text
+            league_title = tr.contents[3].text
+            # print(seq, league_title)
+            if CUP in league_title or JB in league_title:
+                continue  # filter cup match and 锦标赛
+
+            status = tr.contents[7].text
+            if status != END:
+                continue  # filter cancel or delay match
+
+            score = tr.contents[13].text
+            result = getresult(score)
+            home_team = tr.contents[9].text.replace('\n', '')
+            guest_team = tr.contents[15].text.replace('\n', '')
+            analysis_url = base_url + tr.contents[21].contents[2]['href']
+
+            matchResult = getdetail(analysis_url)
+            if matchResult is None:
+                continue  # may be less six match or zero match
+
+            matchResult.seq = seq
+            matchResult.league_title = league_title
+            matchResult.home_team = home_team
+            matchResult.guest_team = guest_team
+            matchResult.score = score
+            matchResult.result = result
+            matchResult.match_day = day.replace('-', '')
+
+            matchResultList.append(matchResult)
+
+        savetoDB(matchResultList)
+        print(day, 'end')
+        time.sleep(3)
+
+def get_match_to_predict(year, month, day):
+    begin = datetime.date(year, month, day)
+    end = datetime.date(year, month, day)
+    matchResultList = []
+    for i in range((end - begin).days + 1):
+        day = begin + datetime.timedelta(days=i)
+        day = str(day)
+        url = base_url + 'buy/jingcai.aspx?typeID=105&oddstype=2&date=' + day
+        strhtml = requests.get(url, headers=Headers)
+        strhtml.encoding = 'utf-8'
+        soup = BeautifulSoup(strhtml.text, 'lxml')
+
+        for tr in soup.find_all(has_attr_cansale):
+            seq = tr.contents[1].text
+            league_title = tr.contents[3].text
+            # print(seq, league_title)
+            if CUP in league_title or JB in league_title:
+                continue  # filter cup match and 锦标赛
+
+            home_team = tr.contents[9].text.replace('\n', '')
+            guest_team = tr.contents[15].text.replace('\n', '')
+            analysis_url = base_url + tr.contents[21].contents[2]['href']
+
+            matchResult = getdetail(analysis_url)
+            if matchResult is None:
+                continue  # may be less six match or zero match
+
+            #match_day = day.replace('-', '')
+            match_day = str(tr.contents[7])[16:26].replace('-', '')
+
+            home_all_rate = round(float(matchResult.home_all_total_score) / float(matchResult.home_all_total_match), 2)
+            home_all_total_get_goal_rate = matchResult.home_all_total_get_goal_rate
+            home_all_total_loss_goal_rate = matchResult.home_all_total_loss_goal_rate
+
+            home_rate = round(float(matchResult.home_total_score) / float(matchResult.home_total_match), 2)
+            home_total_get_goal_rate = matchResult.home_total_get_goal_rate
+            home_total_loss_goal_rate = matchResult.home_total_loss_goal_rate
+
+            guest_all_rate = round(float(matchResult.guest_all_total_score) / float(matchResult.guest_all_total_match), 2)
+            guest_all_total_get_goal_rate = matchResult.guest_all_total_get_goal_rate
+            guest_all_total_loss_goal_rate = matchResult.guest_all_total_loss_goal_rate
+
+            guest_rate = round(float(matchResult.guest_total_score) /  float(matchResult.guest_total_match), 2)
+            guest_total_get_goal_rate = matchResult.guest_total_get_goal_rate
+            guest_total_loss_goal_rate = matchResult.guest_total_loss_goal_rate
+
+            match = [match_day, seq, league_title, home_team, guest_team,
+                     home_all_rate,home_all_total_get_goal_rate,home_all_total_loss_goal_rate,
+                     home_rate,home_total_get_goal_rate,home_total_loss_goal_rate,
+                      guest_all_rate, guest_all_total_get_goal_rate, guest_all_total_loss_goal_rate,
+                      guest_rate, guest_total_get_goal_rate, guest_total_loss_goal_rate]
+
+            matchResultList.append(match)
+
+    # print(matchResultList)
+    columns = ["match_day", "seq","league_title", "home_team" , "guest_team",
+               "home_all_rate", "home_all_total_get_goal_rate", "home_all_total_loss_goal_rate",
+               "home_rate", "home_total_get_goal_rate", "home_total_loss_goal_rate",
+               "guest_all_rate", "guest_all_total_get_goal_rate", "guest_all_total_loss_goal_rate",
+               "guest_rate", "guest_total_get_goal_rate", "guest_total_loss_goal_rate",
+               ]
+
+    dt = pd.DataFrame(matchResultList, columns=columns)
+    root = 'D:\\AI\\ball\\'
+    filename = "predict" + day+".csv"
+    dt.to_csv(root+filename, encoding='utf_8_sig')
+
+    print('end')
+
+
+def main():
+    #get_result(2020, 6, 16, 2020, 6, 18)
+    get_match_to_predict(2020, 6, 19)
+
 
 if __name__ == '__main__':
     main()
