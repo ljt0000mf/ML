6aa77e42987fa69044b3ffe475ae49a42b0326fa
ljt0000mf <ljt0000mf@163.com>
2020-06-21 22:28:33 +0800

    0621

--- pytorch/ball.py
+++ pytorch/ball.py
@@ -2,20 +2,24 @@ import numpy as np
 import lightgbm as lgb
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 ## load data
 root = 'D:\\AI\\ball\\'
 # train_data = pd.read_table(root+'train3.txt') 2 value
-train_data = pd.read_table(root+'3train.txt')  # 3 value
+train_data = pd.read_table(root+'3tain20200531.txt')  # 3 value
 #val_data = pd.read_csv(root+'val.csv')
-predict_data = pd.read_table(root+'predict.txt')
+predict_data = pd.read_table(root+'predict.txt')  # predict0619
 
-
+test_data = pd.read_table(root+'test0531.txt')
 
 trainlabel = np.array(train_data.label)
 traindata = np.array(train_data.drop("label", axis=1))
 
 predictdata = np.array(predict_data)
+
+testlabel = np.array(test_data.label)
+testdata = np.array(test_data.drop("label", axis=1))
 
 # vallabel = np.array(val_data.label)
 # valdata = np.array(val_data.drop("label", axis=1))
@@ -28,8 +32,20 @@ x_train,x_val,y_train,y_val = train_test_split(traindata,trainlabel, test_size=0
 lgb_train = lgb.Dataset(x_train, y_train)
 lgb_val = lgb.Dataset(x_val, y_val, reference=lgb_train)
 
+def get_accuracy(y_preds, testlabel):
 
+    right = 0.0
+    wrong = 0.0
+    for a, b in zip(y_preds, testlabel):
+        if a == b:
+            right = right+1
+        else:
+            wrong = wrong+1
 
+    print("tatal", len(y_preds))
+    print("right", right, ":wrong", wrong)
+    print("accuracy", right/len(y_preds))
+    print("wrong ", wrong/len(y_preds))
 """
 params = {
     'boosting_type': 'gbdt',
@@ -47,38 +63,49 @@ params = {
 }
 """
 params = {
-    'boosting_type': 'dart',
-    'objective': 'multiclass',
-    'num_leaves': 60,
-    'metric': 'multi_logloss',
-    'boost_from_average': 'true',
+    'boosting_type': 'goss',  #  dart goss  gbdt
+    'objective': 'multiclass',  #  multiclass    multiclassova
+    'max_depth': 11, # 9   11
+    'num_leaves': 1533, # 450  1333
+    'metric': {'multi_logloss'},
+    #'metric': {'multi_logloss', 'multi_error'},
     'learning_rate': 0.01,
     'num_class': 3,
-    'feature_fraction': 0.9,
-    'bagging_fraction': 0.8,
-    'bagging_freq': 5,
+    #'feature_fraction': 0.9,
+    #'bagging_fraction': 0.8,
+    #'bagging_freq': 10,
+     #'verbose_eval': 0
+
 }
 
 def train():
     print("开始训练")
-    """
     gbm = lgb.train(params,  # 参数字典
                     lgb_train,  # 训练集
-                    num_boost_round=1000,  # 迭代次数
+                    num_boost_round=2000,  # 迭代次数
                     valid_sets=lgb_val,  # 验证集
-                    early_stopping_rounds=50  # 早停系数
+                    #early_stopping_rounds=50  # 早停系数
                     )
+
     """
-    lgb.cv(params,  # 参数字典
-            lgb_train,  # 训练集
-            num_boost_round=1000,  # 迭代次数
-            nfold=5
-            )
-    # gbm.save_model(root+'model.txt')
+    cv_results = lgb.cv(
+        params, lgb_train, num_boost_round=1000, nfold=5, stratified=False, shuffle=True,
+        early_stopping_rounds=50, verbose_eval=50, show_stdv=True, seed=2020)
+    print(cv_results)
+    print('best n_estimators:', len(cv_results['multi_logloss-mean']))
+    print('best cv score:', cv_results['multi_logloss-mean'][-1])
+    """
+    y_predt = gbm.predict(testdata, num_iteration=gbm.best_iteration)
+    y_preds = [list(x).index(max(x)) for x in y_predt]  # 3 value
+
+    get_accuracy(y_preds, testlabel)
+
+
+    gbm.save_model(root+'model0621.txt')
 
 
 def predict():
-    model_file = root + 'model.txt'
+    model_file = root + 'usemodel\\goss_73_model0621.txt'
     gbm = lgb.Booster(model_file=model_file)
 
     print('开始预测...')
@@ -90,11 +117,11 @@ def predict():
     print(y_preds)
 
     dt = pd.DataFrame(y_preds)
-    dt.to_csv(root + "0619_2result.csv", encoding='utf_8_sig')
+    dt.to_csv(root + "result1.csv", encoding='utf_8_sig')
 
 def main():
-    train()
-    #predict()
+    #train()
+    predict()
 
 
 if __name__ == '__main__':
